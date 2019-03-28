e6a92c8d842f2d16704be7545f85b97afa0ae1c6
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-28 15:08:14 +0000

    Wrapped data in a bookmark object

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,6 +1,13 @@
 require 'pg'
 
 class Bookmark
+  attr_reader :title, :url
+  def initialize(id:, url:, title:)
+    @title = title
+    @id = id
+    @url = url
+  end
+
   def self.create(url:, title:)
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect( dbname: 'bookmark_manager_test' )
@@ -8,7 +15,7 @@ class Bookmark
       connection = PG.connect( dbname: 'bookmark_manager' )
     end
 
-    connection.exec( "INSERT INTO bookmarks (url) VALUES ('#{url}, #{title}')" )
+    connection.exec( "INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}')" )
 
   end
 
@@ -18,10 +25,9 @@ class Bookmark
     else
       connection = PG.connect( dbname: 'bookmark_manager' )
     end
-    
+
     result = connection.exec( "SELECT * FROM bookmarks;" )
-    list = result.map { |bookmark| bookmark['url'] }
-    list.join("\n")
+    list = result.map { |bookmark| Bookmark.new(id: bookmark['id'], url: bookmark['url'], title: bookmark['title'])}
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,23 +6,24 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.destroyallsoftware.com', 'Destroy All Software');")
+      connection.exec("INSERT INTO bookmarks (url, title) VALUES ('http://www.google.com', 'Google');")
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks[0]).to have_attributes(:title => "Makers")
+      expect(bookmarks[1]).to have_attributes(:title => "Destroy All Software")
+      expect(bookmarks[2]).to have_attributes(:title => "Google")
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
       Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
-      expect(Bookmark.all).to include 'http://www.testbookmark.com'
-      expect(Bookmark.all).to include 'Test Bookmark'
+      bookmarks = Bookmark.all
+      expect(bookmarks[0]).to have_attributes(:url => "http://www.testbookmark.com")
+      expect(bookmarks[0]).to have_attributes(:title => "Test Bookmark")
     end
-  end 
+  end
 end
